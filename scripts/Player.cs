8696e420122ab9ee55de4bff6ca57a6d3ead92dc
Yaphina <hegygoodboy@gmail.com>
2024-11-02 19:04:44 +0000

    Enable animations

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -9,17 +9,27 @@ public partial class Player : CharacterBody2D
 	{
 		Vector2 velocity = Velocity;
 
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Play();
+
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
 		if (direction != Vector2.Zero)
 		{
+			animatedSprite2D.Animation = "idle"; // Change this to walk afterwards
 			velocity = direction * Speed;
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		// Changing the player animation to idle when the player is not moving
+		if (velocity == Vector2.Zero)
+		{
+			animatedSprite2D.Animation = "idle";
 		}
 
 		Velocity = velocity;
