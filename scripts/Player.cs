452ec96592c85865d40627175642dc97ec552a81
manofcat <hansseldjc@gmail.com>
2024-11-03 02:44:36 +0000

    change the walking_towards animation later

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,37 +3,42 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	private AnimatedSprite2D animatedSprite2D;
+	private Vector2 velocity;
 	public const float Speed = 300.0f;
+	
+	public override void _Ready()
+	{
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+	public void GetInput()
+	{
+		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
+		velocity = direction * Speed;
+	}
+	
+	public void updateAnimation()
+	{
+		if (velocity.X < 0) {
+			animatedSprite2D.Play("running_left");
+		} else if (velocity.X > 0) {
+			animatedSprite2D.Play("running_right");
+		} else if (velocity.Y < 0) {
+			animatedSprite2D.Play("running_away");
+		} else if (velocity.Y > 0) {
+			animatedSprite2D.Play("running_towards");
+		}
+		
+		if (velocity == Vector2.Zero) {
+			animatedSprite2D.Play("idle");
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
-		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		// if have multiple characters = may need to change name
-		animatedSprite2D.Play();
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
-		if (direction != Vector2.Zero)
-		{
-			animatedSprite2D.Animation = "idle"; // Change this to walk afterwards
-			velocity = direction * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-		}
-
-		// Changing the player animation to idle when the player is not moving
-		if (velocity == Vector2.Zero)
-		{
-			animatedSprite2D.Animation = "idle";
-		}
-
+		GetInput();
 		Velocity = velocity;
 		MoveAndSlide();
+		updateAnimation();
 	}
 }
